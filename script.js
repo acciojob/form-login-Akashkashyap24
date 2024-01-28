2f9a2fb613940467e3caeba175aa1b113255721e
Akashkashyap24 <144315919+Akashkashyap24@users.noreply.github.com>
2024-01-28 14:50:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 function getFormvalue() {
     var firstName = document.getElementById("fname").value;
     var lastName = document.getElementById("lname").value;
-
-    // Now you can use these values as you like
-    // For example, you can display them using alert
-    alert("First Name: " + firstName + ", Last Name: " + lastName);
+	var Submit=document.getElenmentById("Submit").value;
+	Submit.addEventListener("click", myFunction);
+	function myFunction(firstName,lastName) {
+		alert("First Name: " + firstName + ", Last Name: " + lastName);
+	}
 }
