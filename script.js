5eee1b14561a43934ccc9661beaad4b10965f99d
Akashkashyap24 <144315919+Akashkashyap24@users.noreply.github.com>
2024-01-28 14:44:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 function getFormvalue() {
-    //Write your code here
+    var firstName = document.getElementById("fname").value;
+    var lastName = document.getElementById("lname").value;
 
+    // Now you can use these values as you like
+    // For example, you can display them using alert
+    alert("First Name: " + firstName + ", Last Name: " + lastName);
 }
