9c8596656d7e669c540f3818e0dea4cb5614785f
Akashkashyap24 <144315919+Akashkashyap24@users.noreply.github.com>
2024-02-07 01:26:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 function getFormvalue() {
     // Get the input fields
-    var firstName = document.getElementById("fname");
-    var lastName = document.getElementById("lname");
-    var Submit = document.getElementById("submit");
+    var firstName = document.getElementById("fname").value;
+    var lastName = document.getElementById("lname").value;
+    var Submit = document.getElementById("submit").value;
 
     // Add an event listener to the submit button
     Submit.addEventListener("click", function() {
